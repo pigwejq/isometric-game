2b195e1bf01cfc58fa1576582bcf9783b9a936e5
Szymon <szy.chabowski@gmail.com>
2019-01-07 10:46:12 +0100

    just imported assets

---
+++ Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float maxHp = 100f;
+    [SerializeField] float currentHp = 100f;
+    [SerializeField] float maxMp = 100f;
+    [SerializeField] float currentMp = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHp = maxHp;
+        currentMp = maxMp;
+    }
+
+    public float healthAsPercentage
+    {
+        get { return currentHp / maxHp; }
+    }
+
+    public float manaAsPercentage
+    {
+        get { return currentMp / maxMp; }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
